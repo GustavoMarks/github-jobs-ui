2f97381d8164ced55c723a0e8a3e6a47320e4f92
GustavoM7 <gustavosmarquesf@gmail.com>
2020-08-14 20:46:54 -0300

    build: roteamento e principais páginas

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import AllJobs from './pages/AllJobs';
+import HowItWorks from './pages/HowItWorks';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/jobs" component={AllJobs} />
+        <Route exact path="/faq" component={HowItWorks} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
