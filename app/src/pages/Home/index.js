6d1ab0dd4f74d38d79a4ffaa4c062744719920a0
GustavoM7 <gustavosmarquesf@gmail.com>
2020-08-15 12:30:03 -0300

    build: estrutura da home

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -1,5 +1,62 @@
 import React from 'react';
 
-export default function Home(){
-  return <h1> Home </h1>
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <h1> Find the job you want </h1>
+      </header>
+
+      <form>
+        <label> Job Description </label>
+        <input type='text' placeholder='Filter by name, benefits, companies, expertise'/>
+
+        <label> Location </label>
+        <input type='text' placeholder='Filter by city, state, code or country'/>
+
+        <input type="checkbox"/>
+        <label> Full Time Only </label>
+
+        <button type='submit'> Search </button>
+
+      </form>
+
+      <section>
+        <h2> Featured Jobs </h2>
+        <ul>
+          <li> Java Software Developer</li>
+          <li> Full Stack Developer </li>
+        </ul>
+
+        <button> More Awesome Jobs &rarr;</button>
+
+        <h2> Hot Searches </h2>
+        <ul>
+          <li> PHP </li> 
+          <li> Rails </li> 
+          <li> Python </li> 
+          <li> Java Script </li> 
+          <li> Scala </li> 
+          <li> Android </li> 
+          <li> iOS </li>
+          <li> Linux </li>
+          <li> Erlang </li>
+          <li> San Francisco </li>
+          <li> New York City </li>
+          <li> Austin, Tx </li>
+          <li> London </li>
+          <li> Europe </li>
+        </ul>
+
+        <aside>
+          <span> Folow GitHub Jobs on Twitter </span>
+          <div>
+            <span> Featured Company </span>
+            <img src="#" alt="Company" /> 
+            <span> 2 Jobs on Company </span>
+          </div>
+        </aside>
+      </section>
+    </div>
+  )
 }
